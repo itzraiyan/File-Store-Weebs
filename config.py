1cdef4f4abc1e03578868bcdfc47b4e750ee866d
itzraiyan <mdzilhazzarefin@gmail.com>
2024-10-18 11:18:22 +0600

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ except ValueError:
 
 
 
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{filename}<b/>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{filename}</b>")
 
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
