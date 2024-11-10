3193aaa73413b2813f9bed364ff473de73742d16
itzraiyan <mdzilhazzarefin@gmail.com>
2024-11-10 17:30:57 +0600

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -13,20 +13,3 @@ async def stats(bot: Bot, message: Message):
     delta = now - bot.uptime
     time = get_readable_time(delta.seconds)
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
-
-
-
-@Bot.on_message(filters.private & filters.incoming)
-async def useless(_,message: Message):
-    if USER_REPLY_TEXT:
-        await message.reply(USER_REPLY_TEXT)
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
